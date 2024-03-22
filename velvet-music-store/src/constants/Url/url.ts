af4e6fe0951e292d1a1a7872c9d610cbf0fdc523
Omribak <97708552+Omribak@users.noreply.github.com>
2024-03-22 12:10:46 +0200

    changed url

--- velvet-music-store/src/constants/Url/url.ts
+++ velvet-music-store/src/constants/Url/url.ts
@@ -1,9 +1,13 @@
-export const base_url = 'http://localhost:3001/api/v1/';
+export const base_url = 'https://velvet-music-store-back.onrender.com/api/v1/';
 
-export const products_url = 'http://localhost:3001/api/v1/products';
+export const products_url =
+  'https://velvet-music-store-back.onrender.com/api/v1/products';
 
-export const users_url = 'http://localhost:3001/api/v1/users';
+export const users_url =
+  'https://velvet-music-store-back.onrender.com/api/v1/users';
 
-export const cart_url = 'http://localhost:3001/api/v1/cart';
+export const cart_url =
+  'https://velvet-music-store-back.onrender.com/api/v1/cart';
 
-export const checkout_url = 'http://localhost:3001/api/v1/stripe-checkout';
+export const checkout_url =
+  'https://velvet-music-store-back.onrender.com/stripe-checkout';
