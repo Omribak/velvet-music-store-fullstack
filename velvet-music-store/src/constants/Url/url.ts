f39f6167be047efe837497f463012b9ffe16312f
Omribak <97708552+Omribak@users.noreply.github.com>
2024-03-22 19:52:59 +0200

    added backend url

--- velvet-music-store/src/constants/Url/url.ts
+++ velvet-music-store/src/constants/Url/url.ts
@@ -1,13 +1,12 @@
-export const base_url = 'https://velvet-music-store-back.onrender.com/api/v1/';
+export const base_url = 'https://velvet-rosa-backend.onrender.com/api/v1/';
 
 export const products_url =
-  'https://velvet-music-store-back.onrender.com/api/v1/products';
+  'https://velvet-rosa-backend.onrender.com/api/v1/products';
 
 export const users_url =
-  'https://velvet-music-store-back.onrender.com/api/v1/users';
+  'https://velvet-rosa-backend.onrender.com/api/v1/users';
 
-export const cart_url =
-  'https://velvet-music-store-back.onrender.com/api/v1/cart';
+export const cart_url = 'https://velvet-rosa-backend.onrender.com/api/v1/cart';
 
 export const checkout_url =
-  'https://velvet-music-store-back.onrender.com/stripe-checkout';
+  'https://velvet-rosa-backend.onrender.com/stripe-checkout';
